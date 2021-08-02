0f8855cc2596fb9efdcc381b3246817e82978c5d
clysdid <85321892+clysdid@users.noreply.github.com>
2021-08-02 23:35:59 +0900

    Add files via upload

---
+++ KJY/11399_ATM.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+time = 0
+total_time = 0
+
+ppl = list(map(int, input().split()))
+ppl.sort()
+
+for i in range(n):
+    time += ppl[i]
+    total_time += time
+print(total_time)
