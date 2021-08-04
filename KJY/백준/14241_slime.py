d42e16ae2aedf98d81b9575b8055374ffc8a6de7
clysdid <85321892+clysdid@users.noreply.github.com>
2021-08-05 03:47:44 +0900

    Add files via upload

---
+++ KJY/백준/14241_slime.py
@@ -0,0 +1,17 @@
+N = int(input())
+size = list(map(int, input().split()))
+size.sort(reverse=True)  # 최댓값을 구하기 위해, 내림차순으로 정렬
+
+score = 0
+total = 0
+
+for i in range(0, N-1): #슬라임 하나 남을 때까지 반복
+    score = size[i] * size[i+1] # 슬라임을 합칠 때 얻게되는 점수 (x*y)
+    size[i+1] = size[i] + size[i+1]  ## 슬라임 두개가 합쳐지면 다시 1개가 됨. (x+y)
+    total += score
+
+print(total)
+
+
+
+
