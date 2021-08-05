83321f6c19492aa30b223b5e0b7dff2ac84c9623
clysdid <85321892+clysdid@users.noreply.github.com>
2021-08-05 20:23:04 +0900

    Update 14241_slime.py

--- KJY/백준/14241_slime.py
+++ KJY/백준/14241_slime.py
@@ -6,7 +6,7 @@ score = 0
 total = 0
 
 for i in range(0, N-1): #슬라임 하나 남을 때까지 반복
-    score = size[i] * size[i+1] # 슬라임을 합칠 때 얻게되는 점수 (x*y)
+    score = size[i] * size[i+1] # 슬라임을 합칠 때 얻게되는 점수 (x*y)  # 아래의 size[i+1]을 먼저 받으면, 사이즈가 바껴서 순서는 바꾸면 안됨.
     size[i+1] = size[i] + size[i+1]  ## 슬라임 두개가 합쳐지면 다시 1개가 됨. (x+y)
     total += score
 
