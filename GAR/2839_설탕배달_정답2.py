1e331ff7aa82c8adc5f400bda1a4eb4b5a25588f
Areum Ko <78746959+goareum93@users.noreply.github.com>
2021-08-09 02:01:18 +0900

    Algorithm_Study/GAR/greedy

---
+++ GAR/2839_설탕배달_정답2.py
@@ -0,0 +1,15 @@
+N = int(input()) 
+count = 0 
+
+while True: 
+    if (N % 5 == 0): 
+        count = count + (N // 5) 
+        print(count) 
+        break 
+    
+    N = N - 3 
+    count = count + 1 
+    
+    if (N < 0): 
+        print(-1) 
+        break
